fe27e09e2978339fcadb1c0c506c895d34387f52
NataliiaKyrykovych <120452030+NataliiaKyrykovych@users.noreply.github.com>
2023-08-04 15:23:07 -0500

    Update Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
+import { ProfileCard } from "./Profile/ProfileCard";
+import user from './Profile/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
+    <div>
       React homework template
+    < ProfileCard item = {user} />
+      
     </div>
   );
 };
---
+++ src/components/Profile/ProfileCard.jsx
@@ -0,0 +1,28 @@
+export const ProfileCard = ({item: { username, tag, location, avatar, stats: {followers, views, likes } } }) => {
+    
+    return (
+        <div>
+            <div>
+                <img src={avatar} alt="user avatar" />
+                <p> {username} </p>
+                <p>@ {tag} </p>
+                <p> {location} </p>
+            </div>
+            <ul>
+                <li>
+                    <span>Followers</span>
+                    <span> {followers} </span>
+                </li>
+                <li>
+                    <span>Views</span>
+                    <span> {views} </span>
+                </li>
+                <li>
+                    <span>Likes</span>
+                    <span> {likes} </span>
+                </li>
+            </ul>
+        </div>
+    
+    );
+};
